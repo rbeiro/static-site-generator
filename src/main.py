845f6ec2386fc81f461f947dfe2e8ab3f4e83f8b
rbeiro <grsilva112@gmail.com>
2025-05-29 17:00:27 -0300

    feat: change generated files from public dir to docs

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from utils.copystatic import static_to_public
 from utils.generate_page import generate_page_recursive
-
+import sys
 
 def main():
     static_to_public("src/static", "public")
-    generate_page_recursive("content", "template.html", "public")
+    base_path = sys.argv[0]
+    generate_page_recursive("content", "template.html", "docs", base_path)
 
 main()
--- src/utils/generate_page.py
+++ src/utils/generate_page.py
@@ -4,7 +4,7 @@ from utils.extract_markdown import extract_title
 import os
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path="/"):
     print(
         f"Generating page from {from_path} to {dest_path} using {template_path}"
     )
@@ -13,6 +13,8 @@ def generate_page(from_path, template_path, dest_path):
         file_to_string(template_path)
         .replace("{{ Content }}", html)
         .replace("{{ Title }}", title)
+        .replace("href=\"/", f"href=\"{base_path}")
+        .replace("src=\"/", f"src=\"{base_path}")
     )
 
     dest_dir_path = os.path.dirname(dest_path)
@@ -24,16 +26,22 @@ def generate_page(from_path, template_path, dest_path):
 
 
 def generate_page_recursive(
-    dir_path_content: str, template_path, dest_dir_path
+    dir_path_content: str, template_path, dest_dir_path, base_path="/"
 ):
     for dir_content in os.listdir(dir_path_content):
         current_path = os.path.join(dir_path_content, dir_content)
 
         if os.path.isfile(current_path) and dir_content.endswith(".md"):
-            generate_page(current_path, template_path, os.path.join(dest_dir_path, "index.html"))
+            generate_page(
+                current_path,
+                template_path,
+                os.path.join(dest_dir_path, "index.html"),
+            )
         else:
             generate_page_recursive(
-                current_path, template_path, os.path.join(dest_dir_path, dir_content)
+                current_path,
+                template_path,
+                os.path.join(dest_dir_path, dir_content),
             )
 
 
